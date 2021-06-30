910f4b97605f70e84cb51bd9bcb956bd6811ad05
hamzah-sajid-code <82176013+hamzah-sajid-code@users.noreply.github.com>
2021-06-30 19:51:43 +0300

    Update main2.js

--- main2.js
+++ main2.js
@@ -30,7 +30,8 @@ function setup(){
               facingMode: "user"
             }
           });
-    } else{
+    } 
+    if(deviceType == 'mobile'){
         createCapture({
             audio: false,
             video: {
